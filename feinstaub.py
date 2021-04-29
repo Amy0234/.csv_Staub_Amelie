f3310458313ca9af5562ccb6f5e886b41106980c
Amy0234 <02amme34@gmail.com>
2021-04-29 16:48:25 +0200

    das programm2

--- feinstaub.py
+++ feinstaub.py
@@ -1,4 +1,4 @@
 import csv
 csv.list_dialects()
 ['excel', 'excel-tab', 'unix']
-reader = csv.reader(open("sensor_3660.text"), delimiter = ";")
+reader = csv.reader(open("C:\\Users\Amelie Spichartz\.csv_Staub_Amelie\sensor_3660.text"), delimiter = ";")
