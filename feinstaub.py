50a5c956f2bcebdb06f8c81628b2053133f34713
Amy0234 <02amme34@gmail.com>
2021-05-05 14:08:26 +0200

    Das richtige Programm für die Tabelle

--- feinstaub.py
+++ feinstaub.py
@@ -1,4 +1,6 @@
 import csv
-csv.list_dialects()
-['excel', 'excel-tab', 'unix']
-reader = csv.reader(open("C:\\Users\Amelie Spichartz\.csv_Staub_Amelie\sensor_3660.text"), delimiter = ";")
+
+reader = csv.reader(open("C:\\Users\\Amelie Spichartz\\.csv_Staub_Amelie\\sensor_3660.text"), delimiter = ";")
+
+for row in reader:
+    print (f'Zeit: {row [5]} Temperatur: {row [6]} Grad ')
